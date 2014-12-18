2ff357cbc7c61231bc0d22e0232852e9ceb5608e
MaybeMercy <2460572865@qq.c>
2014-12-18 23:22:47 +0800

    增加了云翻译的功能，利用百度翻译ap

--- src/com/example/coolgrammer/SearchActionBar.java
+++ src/com/example/coolgrammer/SearchActionBar.java
@@ -1,29 +1,38 @@
 package com.example.coolgrammer;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.SearchView.OnQueryTextListener;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.ListView;
+
+import com.example.thread.CloudTranslateJsonParsonRunnable;
 /**
  * ActionBar实现搜索
  * @author liandongyang
  * Date:2014.10.13
  */
 public class SearchActionBar extends ActionBarActivity {
+	
+	ListView translate_result;
+	Handler handler;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.cloud_translate);
+		handler = new Handler();
+		initResultView();
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.air_search, menu);//获取menu目录下simple.xml的菜单文件
-		setSearch(menu);
+		setSearchActionBar(menu);
 		setTitle("");
 		return true;
 	}
@@ -31,17 +40,19 @@ public class SearchActionBar extends ActionBarActivity {
 	 * 为ActionBar实现查询功能
 	 * @param menu
 	 */
-	private void setSearch(Menu menu) {
+	private void setSearchActionBar(Menu menu) {
 		// TODO Auto-generated method stub
 		final MenuItem item = menu.findItem(R.id.air_search);
 		item.expandActionView();
 		SearchView sv = (SearchView) MenuItemCompat.getActionView(item);
 		if(sv != null){
+			sv.setSubmitButtonEnabled(true);
 			sv.setOnQueryTextListener(new OnQueryTextListener() {
 				@Override
 				public boolean onQueryTextSubmit(String arg0) {
-					Toast.makeText(getApplicationContext(), " ", Toast.LENGTH_SHORT).show();
 					MenuItemCompat.collapseActionView(item);
+					Thread thread = new Thread(new CloudTranslateJsonParsonRunnable(translate_result, getApplicationContext(), arg0, handler));
+					thread.start();
 					return true;
 				}				
 				@Override
@@ -51,6 +62,7 @@ public class SearchActionBar extends ActionBarActivity {
 			});
 		}
 	}
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
@@ -65,5 +77,9 @@ public class SearchActionBar extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	protected void initResultView(){
+		translate_result = (ListView)findViewById(R.id.trans_result);
 	}
 }
